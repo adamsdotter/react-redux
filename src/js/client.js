b270c50630828a4f0ffc6cd6f47039931bfeda71
padamsson <patricia.adamsson@creuna.se>
2016-09-29 11:02:39 +0200

    Add combined reducers

--- src/js/client.js
+++ src/js/client.js
@@ -1,24 +1,40 @@
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 
-const reducer = function(state, action) {
-  if (action.type === 'INC') {
-    return state + action.payload;
+// it's own file
+const userReducer = (state={}, action) => {
+  switch(action.type) {
+    case 'USER_NAME': {
+      state = {...state, name: action.payload};
+      break;
+    }
+    case 'USER_AGE': {
+      state = {...state, age: action.payload};
+      break;
+    }
   }
-
-  if (action.type === 'DEC') {
-    return state - action.payload;
-  }
-
   return state;
 }
 
-const store = createStore(reducer, 0);
+// it's own file
+const tweetsReducer = (state = [], action) => {
+  return state;
+}
+
+// it's own file
+const reducers = combineReducers({
+  user: userReducer,
+  tweets: tweetsReducer
+});
+
+const store = createStore(reducers);
 
 store.subscribe(() => {
   console.log('store changed: ', store.getState())
 });
 
-store.dispatch({type: 'INC', payload: 2});
-store.dispatch({type: 'INC', payload: 6});
-store.dispatch({type: 'INC', payload: 1});
-store.dispatch({type: 'DEC', payload: 5});
+store.dispatch({type: 'USER_NAME', payload: 'Patricia'});
+store.dispatch({type: 'USER_AGE', payload: 24});
+
+store.dispatch({type: 'USER_NAME', payload: 'Patricia Adamsson'});
+store.dispatch({type: 'USER_AGE', payload: 31});
+store.dispatch({type: 'USER_NAME', payload: 'Patricia Katt'});
