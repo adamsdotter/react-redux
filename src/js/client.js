da1536295b277259794da4f903ca300c70b2106c
padamsson <patricia.adamsson@creuna.se>
2016-09-29 11:08:50 +0200

    Reset to basic setup

--- src/js/client.js
+++ src/js/client.js
@@ -1,40 +1,62 @@
-import { combineReducers, createStore } from 'redux';
+import { createStore } from 'redux';
 
-// it's own file
-const userReducer = (state={}, action) => {
-  switch(action.type) {
-    case 'USER_NAME': {
-      state = {...state, name: action.payload};
-      break;
-    }
-    case 'USER_AGE': {
-      state = {...state, age: action.payload};
-      break;
-    }
-  }
-  return state;
+const reducer = function(state, action) {
+ if (action.type === 'INC') {
+   return state + action.payload;
+ }
+
+ if (action.type === 'DEC') {
+   return state - action.payload;
+ }
+
+ return state;
 }
 
-// it's own file
-const tweetsReducer = (state = [], action) => {
-  return state;
-}
+const store = createStore(reducer, 1);
 
-// it's own file
-const reducers = combineReducers({
-  user: userReducer,
-  tweets: tweetsReducer
-});
+store.dispatch({type: 'INC', payload: 2});
+store.dispatch({type: 'INC', payload: 6});
+store.dispatch({type: 'INC', payload: 1});
+store.dispatch({type: 'DEC', payload: 5});
 
-const store = createStore(reducers);
 
-store.subscribe(() => {
-  console.log('store changed: ', store.getState())
-});
-
-store.dispatch({type: 'USER_NAME', payload: 'Patricia'});
-store.dispatch({type: 'USER_AGE', payload: 24});
-
-store.dispatch({type: 'USER_NAME', payload: 'Patricia Adamsson'});
-store.dispatch({type: 'USER_AGE', payload: 31});
-store.dispatch({type: 'USER_NAME', payload: 'Patricia Katt'});
+// import { combineReducers, createStore } from 'redux';
+//
+// // it's own file
+// const userReducer = (state={}, action) => {
+//   switch(action.type) {
+//     case 'USER_NAME': {
+//       state = {...state, name: action.payload};
+//       break;
+//     }
+//     case 'USER_AGE': {
+//       state = {...state, age: action.payload};
+//       break;
+//     }
+//   }
+//   return state;
+// }
+//
+// // it's own file
+// const tweetsReducer = (state = [], action) => {
+//   return state;
+// }
+//
+// // it's own file
+// const reducers = combineReducers({
+//   user: userReducer,
+//   tweets: tweetsReducer
+// });
+//
+// const store = createStore(reducers);
+//
+// store.subscribe(() => {
+//   console.log('store changed: ', store.getState())
+// });
+//
+// store.dispatch({type: 'USER_NAME', payload: 'Patricia'});
+// store.dispatch({type: 'USER_AGE', payload: 24});
+//
+// store.dispatch({type: 'USER_NAME', payload: 'Patricia Adamsson'});
+// store.dispatch({type: 'USER_AGE', payload: 31});
+// store.dispatch({type: 'USER_NAME', payload: 'Patricia Katt'});
